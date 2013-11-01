559ff39aafa28cf0da1d13fc61578d5f712e7019
Rui Sebastiao <ruisebastiao@gmail.com>
2013-11-01 14:37:39 +0000

    Revision 1.2

--- KPPAutomationCore.cs
+++ KPPAutomationCore.cs
@@ -6,4 +6,30 @@ using System.Windows.Forms;
 
 namespace KPPAutomationCore {
 
+
+    public static class AcessManagement {
+
+        public enum Acesslevel { Admin, User, NotSet, Man }
+
+
+        public delegate void AcesslevelChanged(Acesslevel NewLevel);
+        public static event AcesslevelChanged OnAcesslevelChanged;
+
+
+
+
+        private static Acesslevel _acessLevel = Acesslevel.NotSet;
+
+        public static Acesslevel AcessLevel {
+            get { return _acessLevel; }
+            set {
+                if (_acessLevel != value) {
+                    _acessLevel = value;
+                    if (OnAcesslevelChanged != null) {
+                        OnAcesslevelChanged(value);
+                    }
+                }
+            }
+        }
+    }
 }
