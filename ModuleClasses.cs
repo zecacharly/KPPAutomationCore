fdddf9c5cbbf320bcf18696f01bc9baa10fb1e83
Rui Sebastiao <ruisebastiao@gmail.com>
2013-11-19 08:17:00 +0000

    Revision 3.11
    Added base classes for module settings and projects

--- ModuleClasses.cs
+++ ModuleClasses.cs
@@ -7,13 +7,252 @@ using System.Xml.Serialization;
 using KPP.Core.Debug;
 using IOModule;
 using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+using DejaVu;
+using VisionModule;
+using EpsonModule;
 //using VisionModule;
 
 namespace KPPAutomationCore {
 
     public delegate void SelectedProjectChanged(ModuleProject ProjectSelected);
 
-    //[XmlInclude(typeof(VisionProject))]
+    [XmlInclude(typeof(VisionProjects))]
+    [XmlInclude(typeof(EpsonProjects))]
+    public class ModuleProjects {
+
+        
+        #region -  Serialization attributes  -
+
+        internal static Int32 S_BackupFilesToKeep = 5;
+        internal static String S_BackupFolderName = "backup";
+        internal static String S_BackupExtention = "bkp";
+        internal static String S_DefaulFileExtention = "xml";
+
+        private String _filePath = null;
+        private String _defaultPath = null;
+
+        [XmlIgnore]
+        public Int32 BackupFilesToKeep { get; set; }
+        [XmlIgnore]
+        public String BackupFolderName { get; set; }
+        [XmlIgnore]
+        public String BackupExtention { get; set; }
+
+        #endregion
+
+        private static KPPLogger log = new KPPLogger(typeof(ModuleProjects));
+
+        [XmlAttribute]
+        public virtual String Name { get; set; }
+
+        public string ModuleName {
+            get;
+            set;
+        }
+
+
+
+        public virtual List<ModuleProject> Projects { get; set; }
+
+        
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ModuleProjects() {
+            Name = "Vision Projects";
+            Projects = new List<ModuleProject>();
+        }
+
+
+        //    StaticObjects.ListInspections.Add(item);
+
+        #region Read Operations
+
+        /// <summary>
+        /// Reads the configuration.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns></returns>
+        internal static ModuleProjects ReadConfigurationFile(string path) {
+            //log.Debug(String.Format("Load Xml file://{0}", path));
+            if (File.Exists(path)) {
+                ModuleProjects result = null;
+                TextReader reader = null;
+
+                try {
+                    XmlSerializer serializer = new XmlSerializer(typeof(ModuleProjects));
+                    reader = new StreamReader(path);
+
+                    UndoRedoManager.StartInvisible("Init");
+
+                    ModuleProjects config = serializer.Deserialize(reader) as ModuleProjects;
+
+                    config._filePath = path;
+
+                    result = config;
+                    UndoRedoManager.Commit();
+                } catch (Exception exp) {
+                    log.Error(exp);
+                } finally {
+                    if (reader != null) {
+                        reader.Close();
+                    }
+                }
+                return result;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Reads the configuration.
+        /// </summary>
+        /// <param name="childtype">The childtype.</param>
+        /// <param name="xmlString">The XML string.</param>
+        /// <returns></returns>
+        internal static ModuleProjects ReadConfigurationString(string xmlString) {
+            try {
+                XmlSerializer serializer = new XmlSerializer(typeof(ModuleProjects));
+                ModuleProjects config = serializer.Deserialize(new StringReader(xmlString)) as ModuleProjects;
+
+                return config;
+            } catch (Exception exp) {
+                log.Error(exp);
+            }
+            return null;
+        }
+
+        #endregion
+
+        #region Write Operations
+
+        /// <summary>
+        /// Writes the configuration.
+        /// </summary>
+        public void WriteConfigurationFile() {
+            if (_filePath != null) {
+
+                WriteConfigurationFile(_filePath);
+
+            }
+        }
+
+        /// <summary>
+        /// Writes the configuration.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        public void WriteConfigurationFile(string path) {
+
+            WriteConfiguration(this, path, BackupFolderName, BackupExtention, BackupFilesToKeep);
+
+        }
+
+        /// <summary>
+        /// Writes the configuration string.
+        /// </summary>
+        /// <returns></returns>
+        public String WriteConfigurationToString() {
+
+            return WriteConfigurationToString(this);
+        }
+
+        /// <summary>
+        /// Writes the configuration.
+        /// </summary>
+        /// <param name="config">The config.</param>
+        /// <param name="path">The path.</param>
+        internal static void WriteConfiguration(ModuleProjects config, string path) {
+            WriteConfiguration(config, path, S_BackupFolderName, S_BackupExtention, S_BackupFilesToKeep);
+        }
+
+        /// <summary>
+        /// Writes the configuration.
+        /// </summary>
+        /// <param name="config">The config.</param>
+        /// <param name="path">The path.</param>
+        internal static void WriteConfiguration(ModuleProjects config, string path, string backupFolderName, String backupExtention, Int32 backupFilesToKeep) {
+            if (File.Exists(path) && backupFilesToKeep > 0) {
+                //Do a file backup prior to overwrite
+                try {
+                    //Check if valid backup folder name
+                    if (backupFolderName == null || backupFolderName.Length == 0) {
+                        backupFolderName = "backup";
+                    }
+
+                    //Check Backup folder
+                    String bkpFolder = Path.Combine(Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "Config"), backupFolderName);
+                    if (!Directory.Exists(bkpFolder)) {
+                        Directory.CreateDirectory(bkpFolder);
+                    }
+
+                    //Check extention
+                    String ext = backupExtention != null && backupExtention.Length > 0 ? backupExtention : Path.GetExtension(path);
+                    if (!ext.StartsWith(".")) { ext = String.Format(".{0}", ext); }
+
+                    //Delete existing backup file (This should not exist)
+                    String bkpFile = Path.Combine(bkpFolder, String.Format("{0}_{1:yyyyMMddHHmmss}{2}", Path.GetFileNameWithoutExtension(path), DateTime.Now, ext));
+                    if (File.Exists(bkpFile)) { File.Delete(bkpFile); }
+
+                    //Delete excess backup files
+                    String fileSearchPattern = String.Format("{0}_*{1}", Path.GetFileNameWithoutExtension(path), ext);
+                    String[] bkpFilesList = Directory.GetFiles(bkpFolder, fileSearchPattern, SearchOption.TopDirectoryOnly);
+                    if (bkpFilesList != null && bkpFilesList.Length > (backupFilesToKeep - 1)) {
+                        bkpFilesList = bkpFilesList.OrderByDescending(f => f.ToString()).ToArray();
+                        for (int i = (backupFilesToKeep - 1); i < bkpFilesList.Length; i++) {
+                            File.Delete(bkpFilesList[i]);
+                        }
+                    }
+
+                    //Backup current file
+                    File.Copy(path, bkpFile);
+                    //log.Debug(String.Format("Backup file://{0} to file://{1}", path, bkpFile));
+                } catch (Exception exp) {
+                    //log.Error(String.Format("Error copying file {0} to backup.", path), exp);
+                }
+            }
+            try {
+
+                XmlSerializer serializer = new XmlSerializer(config.GetType());
+                TextWriter textWriter = new StreamWriter(path);
+                serializer.Serialize(textWriter, config);
+                textWriter.Close();
+
+                //log.Debug(String.Format("Write Xml file://{0}", path));
+            } catch (Exception exp) {
+                log.Error("Error writing configuration. ", exp);
+
+                Console.WriteLine(exp.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Writes the configuration to string.
+        /// </summary>
+        /// <param name="config">The config.</param>
+        /// <returns></returns>
+        internal static String WriteConfigurationToString(ModuleProjects config) {
+            try {
+                XmlSerializer serializer = new XmlSerializer(config.GetType());
+                StringWriter stOut = new StringWriter();
+
+                serializer.Serialize(stOut, config);
+
+                return stOut.ToString();
+            } catch (Exception exp) {
+                //log.Error("Error writing configuration. ", exp);
+
+            }
+            return null;
+        }
+
+        #endregion
+
+    }
+
+    [XmlInclude(typeof(VisionProject))]
+    [XmlInclude(typeof(EpsonProject))]
     public class ModuleProject : ICloneable {
 
 
@@ -97,7 +336,10 @@ namespace KPPAutomationCore {
 
         public virtual List<String> ProjectDirectories { get; set; }
 
-       
+        public virtual List<TCPServer> Servers { get; set; }
+
+        [XmlAttribute]
+        public virtual String ProjectFileExtension { get; set; }
 
         [XmlAttribute]
         public virtual String ProjectFile { get; set; }
---
+++ ProjectOptionsForm.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using VisionModule;
+using KPP.Core.Debug;
+using BrightIdeasSoftware;
+using System.Threading;
+using System.Globalization;
+
+namespace KPPAutomationCore {
+
+    public partial class ProjectOptionsForm : Form {
+
+        private ModuleProjects _Projsconf;
+
+        public ModuleProjects Projsconf {
+            get { return _Projsconf; }
+            set { 
+                _Projsconf = value; 
+            }
+        }
+
+        //public ModuleProjects Projsconf = null;
+
+        public String _projsfile = "";
+        private static KPPLogger log = new KPPLogger(typeof(ProjectOptionsForm));
+        Type ModuleType;
+        public ProjectOptionsForm(Type moduleType) {
+            ModuleType = moduleType;
+            switch (LanguageSettings.Language) {
+                case LanguageName.Unk:
+                    break;
+                case LanguageName.PT:
+                    Thread.CurrentThread.CurrentUICulture = new CultureInfo("pt-PT");
+
+                    break;
+                case LanguageName.EN:
+                    Thread.CurrentThread.CurrentUICulture = new CultureInfo("de");
+
+                    break;
+                default:
+                    break;
+            }
+            InitializeComponent();
+           
+        }
+
+        private void ProjectOptionsForm_FormClosing(object sender, FormClosingEventArgs e) {
+            this.Hide();
+            e.Cancel=true;
+        }
+
+
+        private void _btNewProj_Click(object sender, EventArgs e) {
+            
+
+        }
+
+        private void __btsave_Click(object sender, EventArgs e) {
+            Projsconf.WriteConfigurationFile();
+        }
+
+        private void __listprojects_CellEditFinishing(object sender, BrightIdeasSoftware.CellEditEventArgs e) {
+            try {
+                if (e.Cancel) {
+                    return;
+                }
+                
+                if (e.Column == olvLoadOnStart && e.RowObject != null) {
+
+                    Boolean cellvalue = (Boolean)e.NewValue;
+                    String projname = ((ModuleProject)e.RowObject).Name;
+
+                    foreach (ModuleProject item in Projsconf.Projects) {
+                        if (cellvalue == true) {
+                            if (item.Name != projname) {
+                                item.Loadonstart = false;
+                            }
+                        }
+                    }
+
+                    //e.Value
+                }
+                else if (e.Column == olvProjName) {
+                    ((ModuleProject)e.RowObject).Name = (String)e.NewValue;                
+                    
+                } 
+                __listprojects.RefreshObjects(Projsconf.Projects);
+                List<ModuleProject> projects = __listprojects.Objects.Cast<ModuleProject>().ToList();
+                Projsconf.Projects = projects;
+                Projsconf.WriteConfigurationFile(_projsfile);
+            }
+            catch (Exception exp) {
+
+                log.Error(exp);
+            }
+        }
+
+        private void __btduplicate_Click(object sender, EventArgs e) {
+            ModuleProject source = __listprojects.SelectedObject as ModuleProject;
+
+            if (source!=null) {
+                ModuleProject newproj = source.Clone() as ModuleProject;
+                if (newproj!=null) {
+                    newproj.Name = newproj.Name + "_copy";
+                    Projsconf.Projects.Add(newproj);
+                    __listprojects.Objects = Projsconf.Projects;
+                }
+            }
+        }
+
+        private void __listprojects_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode== Keys.Enter) {
+                if (__listprojects.SelectedIndex>0) {
+                    __btLoadProj.PerformClick();
+                }
+            }
+        }
+
+       
+
+        private void ProjectOptionsForm_Load(object sender, EventArgs e) {
+            AcessManagement.OnAcesslevelChanged += new AcessManagement.AcesslevelChanged(StaticObjects_OnAcesslevelChanged);
+            
+        }
+
+        void StaticObjects_OnAcesslevelChanged(Acesslevel NewLevel) {
+            Boolean state = NewLevel ==Acesslevel.Admin;
+
+            olvLoadOnStart.IsVisible = state;
+            olvProjID.IsVisible = state;
+         
+            __btduplicate.Visible = state;
+            __btNewProj.Visible = state;
+
+            __listprojects.RebuildColumns();
+
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e) {
+            if (__listprojects.SelectedIndex==0) {
+                __listprojects.SelectedIndex = __listprojects.Items.Count - 1;
+            } else {
+                __listprojects.SelectedIndex--;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            if (__listprojects.SelectedIndex == __listprojects.Items.Count - 1) {
+                __listprojects.SelectedIndex = 0;
+            } else {
+                __listprojects.SelectedIndex++;
+            }
+        }
+
+        private void __btNewProj_Click(object sender, EventArgs e) {
+            try {
+                // TODO CHECK ModuleProject Type instanciation
+                var newproject = Activator.CreateInstance(ModuleType);
+                Projsconf.Projects.Add((ModuleProject)newproject);
+                __listprojects.Objects=Projsconf.Projects;
+                Projsconf.WriteConfigurationFile(_projsfile);
+
+            } catch (Exception exp) {
+
+                log.Error(exp);
+            }
+        }
+
+      
+    }
+}
